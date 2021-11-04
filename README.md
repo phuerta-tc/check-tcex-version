5ae6b86887a36cde8f973a336447af68274fc09b
Paco Huerta <phuerta@threatconnect.com>
2021-11-03 21:11:35 -0700

    test build that expects pip path

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ Check that TCEX version is up to date.
 
 ## Release Notes
 
-### 0.0.4
+### 0.0.5
 + Initial release
 
--- check_tcex_version/main.py
+++ check_tcex_version/main.py
@@ -1,5 +1,6 @@
 """Check TCEX Version"""
 # standard library
+import argparse
 import sys
 from typing import Optional, Sequence
 
@@ -7,30 +8,42 @@ from typing import Optional, Sequence
 import sh
 
 
+def check_tcex(pip_location):
+    """Check TCEX version"""
+    try:
+        pip_command = sh.Command(pip_location)
+    except Exception:
+        print(f'pip could not be found using provided location: {pip_location}')
+        print('Please provide full path of target pip installation (provide output of "which pip"')
+        return 1
+
+    results = pip_command('list', outdated=True)
+
+    if results.exit_code != 0:
+        print('An unexpected error occurred while listing outdated packages')
+        print(results.stderr.decode())
+        return 1
+
+    output = results.stdout.decode()
+    output = output.splitlines()
+    for line in output:
+        if 'tcex' in line:
+            version = line.split()[-2]
+            print(f'A new version of TCEX is available: {version}')
+            return 1
+    else:
+        print('TCEX is up to date')
+        # return 1 to see if pre-commit prints output on failure
+        return 1
+
+
 def main(argv: Optional[Sequence[str]] = None):
     """Entry point for pre-commit hook."""
-    return_code = 0
-    pip_path = sh.which('pip')
-    print(f'pip: {pip_path}')
-    output = sh.pip('list', outdated=True).strip()
-    print(output)
-    if output is not None:
-        output = output.splitlines()
-        for line in output:
-            if 'tcex' in line:
-                version = line.split()[-2]
-                print(f'A new version of TCEX is available: {version}')
-                return_code = 1
-                break
-        else:
-            print('TCEX is up to date')
-            # return 1 to see if pre-commit prints output on failure
-            return_code = 1
-    else:
-        print('Unable to list outdated packages')
-        return_code = 1
 
-    return return_code
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--pip_location', required=True)
+    args = parser.parse_args(argv)
+    check_tcex(args.pip_location)
 
 
 if __name__ == '__main__':
